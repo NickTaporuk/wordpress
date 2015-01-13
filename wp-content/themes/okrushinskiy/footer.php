10b23e44a34bdad6e0bffcbf0ff918f99e6f0d69
Nick Kuropatkin <nikolay.kuropatkin@gentechpartners.com>
2015-01-13 22:17:09 +0200

    add

---
+++ wp-content/themes/okrushinskiy/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nick
+ * Date: 13.01.15
+ * Time: 10:12
+ */
+?>
+<div class="footer">
+    <div class="ftr-box2 ">
+
+    </div>
+
+    <div class="ftr-bottom">
+        @ Авторские права IMG Partners (все права защищены) 2000-2012
+    </div>
+</div>
+
+
+</div>
+
+</body>
+</html>
---
+++ wp-content/themes/okrushinskiy/sudovaya.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nick
+ * Date: 13.01.15
+ * Time: 22:11
+ */
+
+echo 1111;
