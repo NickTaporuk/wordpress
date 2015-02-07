de6443b6ab8bfdf77379e5ac18efe83c3b22ab5c
NickTaporuk <nictaporuk@yandex.ru>
2015-02-07 21:13:04 +0200

    add

--- wp-content/themes/okrushinskiy/sidebar.php
+++ wp-content/themes/okrushinskiy/sidebar.php
@@ -1,9 +1,9 @@
 <div id="mainmenu">
     <ul class="menu">
         <li class="level-1" id="menuitem-161">
-            <a href="" class="level-1-link" id="i161"
+            <a href="/" class="level-1-link" id="i161"
                onclick="jQuery('#sub-i161').slideToggle(); return false;">Компания</a>
-            <ul class="submenu" id="sub-i161" style="display: none;">
+            <!--<ul class="submenu" id="sub-i161" style="display: none;">
                 <li class="level-2" id="menuitem-164">
                     <a href="http://www.cpk.ua/ru/page/164/">О нас</a>
                 </li>
@@ -19,12 +19,12 @@
                 <li class="level-2" id="menuitem-171">
                     <a href="http://www.cpk.ua/ru/vacancies/">Вакансии</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
         <li class="level-1" id="menuitem-242">
-            <a href="" class="level-1-link" id="i242"
-               onclick="jQuery('#sub-i242').slideToggle(); return false;">Практики</a>
-            <ul class="submenu" id="sub-i242" style="display: none;">
+            <a href="/" class="level-1-link" id="i242"
+               onclick="jQuery('#sub-i242').slideToggle(); return false;">Услуги</a>
+            <!--<ul class="submenu" id="sub-i242" style="display: none;">
                 <li class="level-2" id="menuitem-254">
                     <a href="http://www.cpk.ua/ru/activities/">Юридические услуги</a>
                 </li>
@@ -35,11 +35,11 @@
                     <a href="http://www.cpk.ua/ru/registration/">Регистрация финансовых
                         учреждений</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
         <li class="level-1" id="menuitem-234">
-            <a href="http://www.cpk.ua/ru/tips/" class="level-1-link" id="i234">Полезные советы</a>
-            <ul class="submenu" id="sub-i234" style="display: none;">
+            <a href="/" class="level-1-link" id="i234">Фахівці</a>
+            <!--<ul class="submenu" id="sub-i234" style="display: none;">
                 <li class="level-2" id="menuitem-">
                     <a href="http://www.cpk.ua/ru/tips/3/">Корпоративное управление</a>
                 </li>
@@ -52,11 +52,11 @@
                 <li class="level-2" id="menuitem-">
                     <a href="http://www.cpk.ua/ru/tips/4/">Частным клиентам</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
         <li class="level-1" id="menuitem-174">
-            <a href="http://www.cpk.ua/ru/news/" class="level-1-link" id="i174">Новости</a>
-            <ul class="submenu" id="sub-i174" style="display: none;">
+            <a href="/" class="level-1-link" id="i174">Ціни</a>
+            <!--<ul class="submenu" id="sub-i174" style="display: none;">
                 <li class="level-2" id="menuitem-">
                     <a href="?from=2015">2015</a>
                 </li>
@@ -78,11 +78,11 @@
                 <li class="level-2" id="menuitem-">
                     <a href="?from=2009">2009</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
         <li class="level-1" id="menuitem-175">
-            <a href="http://www.cpk.ua/ru/projects/" class="level-1-link" id="i175">Проекты</a>
-            <ul class="submenu" id="sub-i175" style="display: none;">
+            <a href="/" class="level-1-link" id="i175">Корисні посилання</a>
+            <!--<ul class="submenu" id="sub-i175" style="display: none;">
                 <li class="level-2" id="menuitem-">
                     <a href="http://www.cpk.ua/ru/projects/1/" class="high">M&amp;A, Due Diligence,
                         другие консалтинговые проекты</a>
@@ -96,21 +96,21 @@
                 <li class="level-2" id="menuitem-">
                     <a href="http://www.cpk.ua/ru/projects/3/">Судебная практика</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
         <li class="level-1" id="menuitem-187">
-            <a href="" class="level-1-link" id="i187"
-               onclick="jQuery('#sub-i187').slideToggle(); return false;">Наши клиенты</a>
-            <ul class="submenu" id="sub-i187" style="display: none;">
+            <a href="/" class="level-1-link" id="i187"
+               onclick="jQuery('#sub-i187').slideToggle(); return false;">Контакти</a>
+            <!--<ul class="submenu" id="sub-i187" style="display: none;">
                 <li class="level-2" id="menuitem-191">
                     <a href="http://www.cpk.ua/ru/clients/">Клиенты</a>
                 </li>
                 <li class="level-2" id="menuitem-192">
                     <a href="http://www.cpk.ua/ru/feedback/">Отзывы наших клиентов</a>
                 </li>
-            </ul>
+            </ul>-->
         </li>
-        <li class="level-1" id="menuitem-220">
+        <!--<li class="level-1" id="menuitem-220">
             <a href="" class="level-1-link" id="i220"
                onclick="jQuery('#sub-i220').slideToggle(); return false;">Публикации</a>
             <ul class="submenu" id="sub-i220" style="display: none;">
@@ -130,6 +130,6 @@
         </li>
         <li class="level-1" id="menuitem-193">
             <a href="http://www.cpk.ua/ru/contacts/" class="level-1-link" id="i193">Контакты</a>
-        </li>
+        </li>-->
     </ul>
 </div>
